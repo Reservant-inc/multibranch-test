06f358cb3a714c7c4cd10017a8b87dc0b2190f41
linewelder <redalerm@gmail.com>
2023-12-09 14:23:19 +0100

    Stworzono serwis i model dla WeatherForecast w oddzielnych plikach

---
+++ MultibranchTest/Services/WeatherForecastService.cs
@@ -0,0 +1,25 @@
+using MultibranchTest.Models;
+
+namespace MultibranchTest.Services;
+
+public interface IWeatherForecastService
+{
+    IEnumerable<WeatherForecast> GetWeatherForecast();
+}
+
+public class WeatherForecastService : IWeatherForecastService
+{
+    private static readonly string[] Summaries =
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public IEnumerable<WeatherForecast> GetWeatherForecast() =>
+        Enumerable.Range(1, 5)
+            .Select(index =>
+                new WeatherForecast(
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    Summaries[Random.Shared.Next(Summaries.Length)]
+                ));
+}
